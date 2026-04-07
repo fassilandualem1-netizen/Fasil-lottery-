c68766c6fb03febad205020f97e084c56f8400ae
fassilandualem1-netizen <fassilandualem1@gmail.com>
2026-04-07 04:39:15 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -409,21 +409,23 @@ def handle_secure_pick(call):
     # እዚህ ጋር የተቀነሰውን አዲሱን ሂሳብ እንፈትሻለን
     current_wallet = data["users"][uid]["wallet"]
     
+        # ከዚህ በላይ ያለው Refresh_picker(call, uid, bid) ከሚለው መስመር ጋር 
+    # 'if' እና 'else' እኩል መስመር ላይ መሆን አለባቸው።
+    
     if current_wallet >= board_price:
-        # 🔄 ሰሌዳውን ሳያጠፋ ማደሻ (Function በመጠቀም ኮዱን አሳጥረነዋል)
         refresh_picker(call, uid, bid)
-                else:
+    else:
         # 1. ተጫዋቹ የያዛቸውን ቁጥሮች ዝርዝር ማዘጋጀት
         my_numbers = [num for num, owner in board["slots"].items() if owner == user['name']]
         numbers_str = ", ".join(sorted(my_numbers, key=int))
 
-        # 2. የቆየውን የቁጥር መምረጫ ሰሌዳ ወዲያውኑ ማጥፋት (Clean እንዲሆን)
+        # 2. የቆየውን የቁጥር መምረጫ ሰሌዳ ማጥፋት
         try:
             bot.delete_message(call.message.chat.id, call.message.message_id)
         except:
             pass
 
-        # 3. የደስታ መግለጫ መልዕክቱን ለተጫዋቹ መላክ
+        # 3. የደስታ መግለጫ መልዕክቱን ማዘጋጀት
         success_text = (
             f"🎉 <b>እንኳን ደስ አሎት {user['name']}!</b>\n"
             f"🎫 <b>ቁጥሮችዎን በተሳካ ሁኔታ መርጠው ጨርሰዋል።</b>\n\n"
@@ -431,18 +433,18 @@ def handle_secure_pick(call):
             f"━━━━━━━━━━━━━━━━━━━━━\n"
             f"✨ <b>መልካም ዕድል ይሁንሎት! 🏆</b>"
         )
-        
-        # መልዕክቱን ግሩፕ ላይ መላክ
+
+        # ግሩፕ ላይ መላክ
         sent_msg = bot.send_message(GROUP_ID, success_text, parse_mode="HTML")
 
-        # 4. 🛑 መልዕክቱን ከ10 ሰከንድ በኋላ የሚያጠፋው ብልህ ዘዴ
+        # 4. ከ10 ሰከንድ በኋላ መልዕክቱን የሚያጠፋ ፈንክሽን
         def delete_later(chat_id, message_id):
             try:
                 bot.delete_message(chat_id, message_id)
             except:
                 pass
 
-        # threading በመጠቀም ለ10 ሰከንድ ቀጠሮ መያዝ (ከላይ import threading ማለታችሁን አረጋግጡ)
+        # threading በመጠቀም ለ10 ሰከንድ ቀጠሮ መያዝ
         import threading
         threading.Timer(10, delete_later, args=[GROUP_ID, sent_msg.message_id]).start()
 
