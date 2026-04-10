a9835c4f4aa4b71e59862e765eac30b437275e20
fassilandualem1-netizen <fassilandualem1@gmail.com>
2026-04-10 05:25:45 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ def home(): return "I am alive!"
 def run(): app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
 
 # ቦቱ መስራቱን ቼክ ማድረጊያ
-TOKEN = "የ@Fasil_assistant_bot_ትክክለኛ_token_እዚህ_ይግባ"
+TOKEN = "8757888085:AAHorIou6gWJjUgvgNeJlC2HbkNq2Wohizs"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
