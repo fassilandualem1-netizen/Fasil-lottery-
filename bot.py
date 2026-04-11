d75d9eb3dad61d9ac7f1d1b9269b8849ddadfef2
fassilandualem1-netizen <fassilandualem1@gmail.com>
2026-04-11 01:24:56 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
 import telebot
 import time
 
-# ቶከኑን እዚህ ጋር በጥንቃቄ ለጥፍ
+# አዲሱ ቶከንህ
 TOKEN = "8757888085:AAHHv6ne3gDNeO-d8TjX2ag26bQrT53Z28Q"
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "ቦቱ በሰላም ሰርቷል!")
+    bot.reply_to(message, "✅ ፋሲል ቦት አሁን በDocker በኃይል ተነስቷል!")
 
 if __name__ == "__main__":
-    print("ቦቱ እየጀመረ ነው...")
+    print("Bot is starting...")
     bot.remove_webhook()
     time.sleep(1)
     bot.infinity_polling()
