83e9a373425ef2a86ac11103e9c0a2efbb1a1ec8
fassilandualem1-netizen <fassilandualem1@gmail.com>
2026-04-15 02:43:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -174,6 +174,9 @@ def get_admin_dashboard():
 # አድሚኑ /admin ሲል የሚመጣ መልዕክት
 @bot.message_handler(commands=['admin'])
 def show_admin_panel(message):
+    # 1. መጀመሪያ ማንኛውንም ተከታታይ ጥያቄ (Step Handler) ያጠፋል
+    bot.clear_step_handler_by_chat_id(chat_id=message.chat.id)
+    
     if message.from_user.id in ADMIN_IDS:
         bot.send_message(
             message.chat.id, 
