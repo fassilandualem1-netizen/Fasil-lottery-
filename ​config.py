c9a221d9515bef35de078acad7786c636a8bff69
fassilandualem1-netizen <fassilandualem1@gmail.com>
2026-07-16 21:48:56 +0300

    Update ​config.py

--- ​config.py
+++ ​config.py
@@ -1,1 +1,18 @@
-y
+import os
+import telebot
+from upstash_redis import Redis
+
+# ==========================================
+# ⚙️ Configuration (ማዋቀሪያዎች)
+# ==========================================
+TOKEN = os.environ.get("BOT_TOKEN")
+REDIS_URL = os.environ.get("REDIS_URL")
+REDIS_TOKEN = os.environ.get("REDIS_TOKEN")
+WEB_APP_URL = "https://sefer-bot.onrender.com"
+ADMIN_ID = 8488592165  # የአድሚን ቴሌግራም ID
+
+# 🤖 የቴሌግራም ቦት ማስነሻ
+bot = telebot.TeleBot(TOKEN, parse_mode="HTML", threaded=False)
+
+# 💾 የUpstash Redis ዳታቤዝ ግንኙነት
+redis = Redis(url=REDIS_URL, token=REDIS_TOKEN)
