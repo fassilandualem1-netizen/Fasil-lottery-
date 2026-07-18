fd9aeeb51bc38b06e6335223d49c370be1197f94
fassilandualem1-netizen <fassilandualem1@gmail.com>
2026-07-18 20:58:48 +0300

    Update real_sports.js

--- static/js/real_sports.js
+++ static/js/real_sports.js
@@ -1,1 +1,7 @@
-g
+async function fetchMatches() {
+    const res = await fetch('/api/get-real-matches');
+    const result = await res.json();
+    console.log(result.data); // ጨዋታዎቹ እዚህ ይመጣሉ
+    // አሁን እነዚህን ጨዋታዎች በ Loop ተጠቅመህ real_sports.html ላይ ማሳየት ትችላለህ
+}
+fetchMatches();
